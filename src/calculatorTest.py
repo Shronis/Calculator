e4ec30b9e4877f582604568f68e1499dc7b97a99
Shronis <lmincorvaia@gmail.com>
2021-06-30 15:44:42 -0400

    Feature:Added add method

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -7,9 +7,13 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
-    def test_results_property_calculator(self):
+    def test_results_property_Calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.result, 4)
+
+    def test_add_method_Calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2,2), 4)
 
 
 if __name__ == '__main__':
