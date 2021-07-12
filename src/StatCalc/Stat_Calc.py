f8f7aa57254973eab7c27b97635b57b7b8d32083
Shronis <lmincorvaia@gmail.com>
2021-07-12 13:30:12 -0400

    Added Mode Function

--- src/StatCalc/Stat_Calc.py
+++ src/StatCalc/Stat_Calc.py
@@ -1,6 +1,7 @@
 from Calculator.calculator import Calculator
 from StatCalc.Mean import mean
 from StatCalc.Median import median
+from StatCalc.Mode import mode
 
 class Statistics(Calculator):
     data = []
@@ -14,4 +15,8 @@ class Statistics(Calculator):
 
     def median(self, data):
         self.result = median(data)
+        return self.result
+
+    def mode(self, data):
+        self.result = mode(data)
         return self.result
