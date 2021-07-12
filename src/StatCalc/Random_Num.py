bc1f3c9adfa4ed7da1572ec19957213d83024524
Shronis <lmincorvaia@gmail.com>
2021-07-12 15:23:10 -0400

    Added Random number generator w/ and w/ out seed and list verion

---
+++ src/StatCalc/Random_Num.py
@@ -0,0 +1,5 @@
+import random
+import decimal
+
+def random_num():
+    return decimal.Decimal(str(random.random()))
--- src/StatCalc/Stat_Calc.py
+++ src/StatCalc/Stat_Calc.py
@@ -4,6 +4,10 @@ from StatCalc.Median import median
 from StatCalc.Mode import mode
 from StatCalc.Variance import variance
 from StatCalc.StdDev import stddev
+from StatCalc.RandomSeed import random_seed
+from StatCalc.Random_Num import random_num
+from StatCalc.Random_Number_List import random_num_list
+
 
 class Statistics(Calculator):
     data = []
@@ -29,4 +33,16 @@ class Statistics(Calculator):
 
     def stddev(self, data):
         self.result = stddev(data)
+        return self.result
+
+    def random_seed(self, x,y):
+        self.result = random_seed(x,y)
+        return self.result
+
+    def random_seed(self):
+        self.result = random_num()
+        return self.result
+
+    def random_seed(self, x,y,n):
+        self.result = random_num_list(x,y,n)
         return self.result
