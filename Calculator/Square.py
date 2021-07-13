66c8d0fddf276baf2dd1d5b81f9b470a0dc37357
Shronis <57580632+Shronis@users.noreply.github.com>
2021-07-12 20:52:54 -0400

    Update Square.py

--- Calculator/Square.py
+++ Calculator/Square.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 def square(a):
     try:
         return float(a)**2
     except ZeroDivisionError:
         print("Can't Divide by 0")
     except ValueError:
-=======
-def square(a):
-    try:
-        return float(a)**2
-    except ZeroDivisionError:
-        print("Can't Divide by 0")
-    except ValueError:
->>>>>>> origin/master
         print("Invalid Data")
