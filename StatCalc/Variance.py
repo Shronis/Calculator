fc664660b3f451dc09d4d200266719323e9f314a
Shronis <57580632+Shronis@users.noreply.github.com>
2021-07-12 20:51:30 -0400

    Update Variance.py

--- StatCalc/Variance.py
+++ StatCalc/Variance.py
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-from Calculator.Square import square
-from Calculator.Division import division
-from StatCalc.Mean import mean
 
-
-def variance(x):
-    try:
-        meanx = mean(x)
-        num_values = len(x)
-        k = 0
-        for i in x:
-            k = k + square(i-meanx)
-        return division(k, num_values)
-    except ZeroDivisionError:
-        print("Can't Divide by 0")
-    except ValueError:
-=======
 from Calculator.Square import square
 from Calculator.Division import division
 from StatCalc.Mean import mean
