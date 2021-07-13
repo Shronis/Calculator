65a651ba99a0bf5eeaf9fa36804aef548a4c9eb2
Shronis <57580632+Shronis@users.noreply.github.com>
2021-07-12 20:52:38 -0400

    Update Root.py

--- Calculator/Root.py
+++ Calculator/Root.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 def squareroot(a):
     try:
         return round((float(a)**.5), 9)
     except ZeroDivisionError:
         print("Can't Divide by 0")
     except ValueError:
-=======
-def squareroot(a):
-    try:
-        return round((float(a)**.5), 9)
-    except ZeroDivisionError:
-        print("Can't Divide by 0")
-    except ValueError:
->>>>>>> origin/master
         print("Invalid Data")
