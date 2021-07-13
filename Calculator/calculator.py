a1d8fd0637cf96ba1f3f985a09702dc76e2881a8
Shronis <57580632+Shronis@users.noreply.github.com>
2021-07-12 20:53:38 -0400

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from csvReader import CsvReader
 from Calculator.Subtraction import subtraction
 from Calculator.Addition import addition
@@ -63,70 +63,4 @@ class CsvStats(Calculator):
         pass
 
     def mean(self):
-=======
-from csvReader import CsvReader
-from Calculator.Subtraction import subtraction
-from Calculator.Addition import addition
-from Calculator.Multiplication import multiplication
-from Calculator.Division import division
-from Calculator.Square import square
-from Calculator.Root import squareroot
-
-class Calculator:
-    result = 0
-
-    def __init__(self):
-        x = 2 + 2
-        self.result = x;
-        pass
-
-    def add(self,a,b):
-        self.result = addition(a,b)
-        return self.result
-
-    def sub(self,a,b):
-        self.result = subtraction(a,b)
-        return self.result
-
-    def mul(self,a,b):
-        self.result = multiplication(a,b)
-        return self.result
-
-    def div(self,a,b):
-        self.result = division(a,b)
-        return self.result
-
-    def sqr(self,a):
-        self.result = square(a)
-        return self.result
-
-    def sqrt(self,a):
-        self.result = squareroot(a)
-        return self.result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class CsvStats(Calculator):
-    data = []
-
-    def __init__(self, data_file):
-        self.data = CsvReader(data_file)
-        pass
-
-    def mean(self):
->>>>>>> origin/master
         mean(self.data)
