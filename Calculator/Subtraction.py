48d34aff6994fb438f5a33c9689e0cafd0a5065b
Shronis <57580632+Shronis@users.noreply.github.com>
2021-07-12 22:07:17 -0400

    Update Subtraction.py

--- Calculator/Subtraction.py
+++ Calculator/Subtraction.py
@@ -1,7 +1,7 @@
 
 def subtraction(a, b):
     try:
-        return float(a) - float(b)
+        return float(b) - float(a)
     except ZeroDivisionError:
         print("Can't Divide by 0")
     except ValueError:
