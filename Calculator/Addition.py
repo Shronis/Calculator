e6579c11006ced6c01713a3fd1453339a4a3031e
Shronis <57580632+Shronis@users.noreply.github.com>
2021-07-12 20:51:50 -0400

    Update Addition.py

--- Calculator/Addition.py
+++ Calculator/Addition.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def addition(a, b):
 
     try:
@@ -6,13 +6,4 @@ def addition(a, b):
     except ZeroDivisionError:
         print("Can't Divide by 0")
     except ValueError:
-=======
-def addition(a, b):
-
-    try:
-        return float(a) + float(b)
-    except ZeroDivisionError:
-        print("Can't Divide by 0")
-    except ValueError:
->>>>>>> origin/master
         print("Invalid Data")
